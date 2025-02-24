155c842e3df167aee79c2b6d2a21291b8fa72d98
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-02-24 21:17:17 +0700

    update connection to database

--- src/backend/config/configMongodb.js
+++ src/backend/config/configMongodb.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 
 const configMongodb = (URI) => {
   mongoose
-    .connect(URI)
+    .connect(URI, {
+      // if mongodb server is down, after 5s your mongoose.connect()
+      // call will only throw an error
+      serverSelectionTimeoutMS: 5000
+    })
     .then(() => {
-      console.log("\n>>> Database connected successful.");
+      console.log("\n### Database connected successful");
     })
     .catch((error) => {
-      console.log(`\n>>> Database connection have error:\n${error}.`);
+      console.log(`\n### Database connection have error:\n${error}`);
       throw error;
     });
 };
