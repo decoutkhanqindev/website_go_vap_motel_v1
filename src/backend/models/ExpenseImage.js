e19ea0e9d916726c225675e3af636cee88c6c6c4
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-04 20:00:40 +0700

    add image model

---
+++ src/backend/models/ExpenseImage.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const expenseImageSchema = new Schema(
+  {
+    expenseId: {
+      type: ObjectId,
+      ref: "Expense",
+      required: true
+    },
+    data: {
+      type: buffer,
+      required: true
+    },
+    contentType: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    collection: "expenseImages",
+    versionKey: false,
+    timestamps: true
+  }
+);
+
+const ExpenseImage = mongoose.model("ExpenseImage", expenseImageSchema);
+
+module.exports = ExpenseImage;
---
+++ src/backend/models/RoomImage.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const roomImageSchema = new Schema(
+  {
+    roomId: {
+      type: ObjectId,
+      ref: "Room",
+      required: true
+    },
+    data: {
+      type: Buffer,
+      required: true
+    },
+    contentType: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    collection: "roomImages",
+    versionKey: false,
+    timestamps: true
+  }
+);
+
+const RoomImage = mongoose.model("RoomImage", roomImageSchema);
+
+module.exports = RoomImage;
