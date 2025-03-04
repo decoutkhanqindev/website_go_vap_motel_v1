ac0f9caa361798751b535029e86722a00f0e7be4
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-05 00:12:35 +0700

    update project

--- src/backend/app.js
+++ src/backend/app.js
@@ -11,8 +11,8 @@ const env = process.env;
 const PORT = env.PORT || 8080;
 const MONGO_DB_URI = env.MONGO_DB_URI;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(requestLoggingMiddleware); // middleware handle request Logging
 app.use("/api", router);
 app.use(errorHandlingMiddleware); // middleware handle common error
