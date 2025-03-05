c89d040a5eaa19991ae847883e098bdefcd4401f
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-05 20:22:30 +0700

    add utility controller

---
+++ src/backend/controllers/UtilityController.js
@@ -0,0 +1,150 @@
+const ApiError = require("../utils/ApiError");
+const logger = require("../utils/logger");
+const UtilityService = require("../services/UtilityService");
+
+class UtilityController {
+  static async getAllUtilities(req, res, next) {
+    try {
+      logger.info("UtilityController.getAllUtilities() is called.");
+      const utilities = await UtilityService.getAllUtilities();
+      res.status(200).json(utilities);
+    } catch (error) {
+      logger.error(`UtilityController.getAllUtilities() have error:\n${error}`);
+      next(error);
+    }
+  }
+
+  static async getUtilityById(req, res, next) {
+    try {
+      logger.info("UtilityController.getUtilityById() is called.");
+      const id = req.params.id;
+      if (!id) {
+        return next(new ApiError(400, "Param must be provided."));
+      }
+      const utility = await UtilityService.getUtilityById(id);
+      res.status(200).json(utility);
+    } catch (error) {
+      logger.error(`UtilityController.getUtilityById() have error:\n${error}`);
+      next(error);
+    }
+  }
+
+  static async getUtilityImageById(req, res, next) {
+    try {
+      logger.info("UtilityController.getUtilityImageById() is called.");
+      const id = req.params.id;
+      if (!id) {
+        return next(new ApiError(400, "Param must be provided."));
+      }
+      const utilityImage = await UtilityService.getUtilityImageById(id);
+      res.status(200).json(utilityImage);
+    } catch (error) {
+      logger.error(
+        `UtilityController.getUtilityImageById() have error:\n${error}`
+      );
+      next(error);
+    }
+  }
+
+  static async addImagesToUtility(req, res, next) {
+    try {
+      logger.info("UtilityController.addImagesToUtility() is called.");
+      const id = req.params.id;
+      const imageFiles = req.files;
+
+      if (!id) {
+        return next(new ApiError(400, "Param must be provided."));
+      }
+
+      if (imageFiles && imageFiles.length === 0) {
+        return next(new ApiError(400, "At least one image must be added."));
+      }
+
+      const updatedUtility = await UtilityService.addImagesToUtility(
+        id,
+        imageFiles
+      );
+      res.status(201).json(updatedUtility);
+    } catch (error) {
+      logger.error(
+        `UtilityController.addImagesToUtility() have error:\n${error}`
+      );
+      next(error);
+    }
+  }
+
+  static async deleteImageForUtility(req, res, next) {
+    try {
+      logger.info("UtilityController.deleteImagesToUtility() is called.");
+      const { id, imageId } = req.params;
+      if (!id || !imageId) {
+        return next(new ApiError(400, "Params must be provided."));
+      }
+      const updatedUtility = await UtilityService.deleteImageForUtility(
+        id,
+        imageId
+      );
+      res.status(201).json(updatedUtility);
+    } catch (error) {
+      logger.error(
+        `UtilityController.deleteImagesToUtility() have error:\n${error}`
+      );
+      next(error);
+    }
+  }
+
+  static async addNewUtility(req, res, next) {
+    try {
+      logger.info("UtilityController.addNewUtility() is called.");
+      const data = req.body;
+      const imageFiles = req.files;
+      if (!data.name && !data.price) {
+        return next(new ApiError(400, "No form data found."));
+      }
+      const addedUtility = await UtilityService.addNewUtility(data, imageFiles);
+      res.status(201).json(addedUtility);
+    } catch (error) {
+      logger.error(`UtilityController.addNewUtility() have error:\n${error}`);
+      next(error);
+    }
+  }
+
+  static async updateUtility(req, res, next) {
+    try {
+      logger.info("UtilityController.updateUtility() is called.");
+      const id = req.params.id;
+      const data = req.body;
+
+      if (!id) {
+        return next(new ApiError(400, "Param must be provided."));
+      }
+
+      if (!data.name && !data.price) {
+        return next(new ApiError(400, "At least one field must be updated."));
+      }
+
+      const updatedUtility = await UtilityService.updateUtility(id, data);
+      res.status(200).json(updatedUtility);
+    } catch (error) {
+      logger.error(`UtilityController.updateUtility() have error:\n${error}`);
+      next(error);
+    }
+  }
+
+  static async deleteUtility(req, res, next) {
+    try {
+      logger.info("UtilityController.deleteUtility() is called.");
+      const id = req.params.id;
+      if (!id) {
+        return next(new ApiError(400, "Param must be provided."));
+      }
+      const deletedUtility = await UtilityService.deleteUtility(id);
+      res.status(200).json(deletedUtility);
+    } catch (error) {
+      logger.error(`UtilityController.deleteUtility() have error:\n${error}`);
+      next(error);
+    }
+  }
+}
+
+module.exports = UtilityController;
