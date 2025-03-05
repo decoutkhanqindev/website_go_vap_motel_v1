a8b25f83bb7772dd8b50683fb125e6b083a4a599
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-05 20:22:59 +0700

    add amenity and utility routes

--- src/backend/routes/apiRoute.js
+++ src/backend/routes/apiRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../config/uploadConfig");
 const AmenityController = require("../controllers/AmenityController");
+const UtilityController = require("../controllers/UtilityController");
 const RoomController = require("../controllers/RoomController");
 
 // amenity routes
@@ -14,16 +15,43 @@ router.post(
 );
 router.put("/amenity/:id", AmenityController.updateAmenity);
 router.delete("/amenity/:id", AmenityController.deleteAmenity);
+// amenity image routes
 router.get("/amenity/image/:id", AmenityController.getAmenityImageById);
-router.post(
+router.put(
   "/amenity/:id/images",
   upload.array("images", 5),
   AmenityController.addImagesToAmenity
 );
-router.delete(
-  "/amenity/:id/image/:imagesId",
-  AmenityController.deleteImagesToAmenity
+router.put(
+  "/amenity/:id/image/:imageId",
+  AmenityController.deleteImageForAmenity
 );
+
+// ___________________________________________________________________________________________________
+
+// utility routes
+router.get("/utilities", UtilityController.getAllUtilities);
+router.get("/utility/:id", UtilityController.getUtilityById);
+router.post(
+  "/utility",
+  upload.array("images", 5),
+  UtilityController.addNewUtility
+);
+router.put("/utility/:id", UtilityController.updateUtility);
+router.delete("/utility/:id", UtilityController.deleteUtility);
+// utility image routes
+router.get("/utility/image/:id", UtilityController.getUtilityImageById);
+router.put(
+  "/utility/:id/images",
+  upload.array("images", 5),
+  UtilityController.addImagesToUtility
+);
+router.put(
+  "/utility/:id/image/:imageId",
+  UtilityController.deleteImageForUtility
+);
+
+// ___________________________________________________________________________________________________
 
 // room routes
 router.get("/rooms", RoomController.getAllRooms);
