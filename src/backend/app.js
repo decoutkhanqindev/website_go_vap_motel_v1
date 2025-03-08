327f587dcf267680fa4079af73562efccfdb92e2
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-08 14:46:30 +0700

    update config app

--- src/backend/app.js
+++ src/backend/app.js
@@ -13,12 +13,17 @@ const env = process.env;
 const PORT = env.PORT || 8080;
 const MONGO_DB_URI = env.MONGO_DB_URI;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(cookieParser());
+app.use(express.json()); // middleware parses incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })); // middleware parses incoming requests with URL-encoded payloads
+app.use(
+  cors({
+    credentials: true,
+    origin: `http://localhost:${PORT}`
+  })
+); // middleware enables Cross-Origin Resource Sharing (CORS)
+app.use(cookieParser()); // middleware parses cookies attached to incoming requests
 app.use(requestLoggingMiddleware); // middleware handle request Logging
-app.use("/api", router);
+app.use("/api", router); // middleware handle all api routes
 app.use(errorHandlingMiddleware); // middleware handle common error
 
 app.listen(PORT, () => {
