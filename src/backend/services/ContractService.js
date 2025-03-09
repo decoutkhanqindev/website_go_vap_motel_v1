e8ddc0628bfe3170a2561bfb81cfe64a0f9b93de
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-10 00:15:35 +0700

    git add contract service

---
+++ src/backend/services/ContractService.js
@@ -0,0 +1,176 @@
+const ApiError = require("../utils/ApiError");
+const logger = require("../utils/logger");
+const moment = require("moment-timezone");
+const Contract = require("../models/Contract");
+
+class ContractService {
+  static async getAllContracts(filter) {
+    try {
+      logger.info(`ContractService.getAllContracts() is called.`);
+      let query = Contract.find();
+
+      if (filter.roomId) query = query.where("roomId").equals(filter.roomId);
+      if (filter.contractCode)
+        query = query.where("contractCode").equals(filter.contractCode);
+      if (filter.startDate)
+        query = query.where("startDate").equals(filter.startDate);
+      if (filter.endDate) query = query.where("endDate").equals(filter.endDate);
+      if (filter.status) query = query.where("status ").equals(filter.status);
+
+      const contracts = await query;
+      if (!contracts.length) {
+        throw new ApiError(404, `No contracts found matching your filter.`);
+      }
+      return contracts;
+    } catch (error) {
+      logger.info(`ContractService.getAllContracts() have error:\n${error}`);
+      throw error;
+    }
+  }
+
+  static async getContractById(id) {
+    try {
+      logger.info(`ContractService.getContractById() is called.`);
+      const contract = await Contract.findById(id);
+      if (!contract) {
+        throw new ApiError(404, `No contract found matching id ${id}.`);
+      }
+      return contract;
+    } catch (error) {
+      logger.info(`ContractService.getContractById() have error:\n${error}`);
+      throw error;
+    }
+  }
+
+  static async isExsits(roomId) {
+    try {
+      logger.info(`ContractService.isExsits() is called.`);
+      const contract = await Contract.findOne({ roomId: roomId });
+      if (!contract) return false;
+      return true;
+    } catch (error) {
+      logger.info(`ContractService.isExsits() have error:\n${error}`);
+      throw error;
+    }
+  }
+
+  static async generateContractCode(startDate, endDate) {
+    try {
+      logger.info(`ContractService.generateContractCode() is called.`);
+      const formatStartDate = moment(startDate).format("YYYYMMDD");
+      const formatEndDate = moment(endDate).format("YYYYMMDD");
+
+      const lastContract = await Contract.findOne().sort({ _id: -1 });
+      if (!lastContract) return `HD${formatStartDate}${formatEndDate}001`;
+
+      const lastContractCode = lastContract.contractCode;
+      const lastContractNumber = parseInt(lastContractCode.slice(-3));
+      const newContractNumber = lastContractNumber + 1;
+      const newContractCode = `HD${formatStartDate}${formatEndDate}${newContractNumber
+        .toString()
+        .padStart(3, "0")}`;
+      return newContractCode;
+    } catch (error) {
+      logger.info(
+        `ContractService.generateContractCode() have error:\n${error}`
+      );
+      throw error;
+    }
+  }
+
+  static async addNewContract(data) {
+    try {
+      logger.info("ContractService.addNewContract() is called.");
+      const { roomId, contractCode, startDate, endDate } = data;
+
+      const isExits = await ContractService.isExsits(roomId);
+      if (isExits) throw new ApiError(409, "This contract already exists.");
+
+      if (!contractCode) {
+        data.contractCode = await ContractService.generateContractCode(
+          startDate,
+          endDate
+        );
+      }
+
+      const newContract = new Contract(data);
+      const addedContract = await newContract.save();
+      return addedContract;
+    } catch (error) {
+      logger.error(`ContractService.addNewContract() have error:\n${error}`);
+      throw error;
+    }
+  }
+
+  static async deleteContract(id) {
+    try {
+      logger.info("ContractService.deleteContract() is called.");
+      const deletedContract = await Contract.findByIdAndDelete(id);
+      if (!deletedContract) {
+        throw new ApiError(400, `No contract found matching id ${id}.`);
+      }
+      return deletedContract;
+    } catch (error) {
+      logger.error(`ContractService.deleteContract() have error:\n${error}`);
+      throw error;
+    }
+  }
+
+  static async extendContract(id, newEndDate) {
+    try {
+      logger.info("ContractService.extendContract() is called.");
+      const updatedContract = await Contract.findByIdAndUpdate(
+        id,
+        { endDate: newEndDate },
+        { new: true }
+      );
+      if (!updatedContract) {
+        throw new ApiError(404, `No contract found matching id ${id}.`);
+      }
+      return updatedContract;
+    } catch (error) {
+      logger.error(`ContractService.extendContract() have error:\n${error}`);
+      throw error;
+    }
+  }
+
+  static async updateContractStatus(id, status) {
+    try {
+      logger.info("ContractService.updateContractStatus() is called.");
+      const updatedContract = await Contract.findByIdAndUpdate(
+        id,
+        { status: status },
+        { new: true }
+      );
+      if (!updatedContract) {
+        throw new ApiError(400, `No contract found matching id ${id}.`);
+      }
+      return updatedContract;
+    } catch (error) {
+      logger.error(
+        `ContractService.updateContractStatus() have error:\n${error}`
+      );
+      throw error;
+    }
+  }
+
+  static async terminateContract(id) {
+    try {
+      logger.info("ContractService.terminateContract() is called.");
+      const updatedContract = await Contract.findByIdAndUpdate(
+        id,
+        { status: "terminated" },
+        { new: true }
+      );
+      if (!updatedContract) {
+        throw new ApiError(404, `No contract found matching id ${id}.`);
+      }
+      return updatedContract;
+    } catch (error) {
+      logger.error(`ContractService.terminateContract() have error:\n${error}`);
+      throw error;
+    }
+  }
+}
+
+module.exports = ContractService;
