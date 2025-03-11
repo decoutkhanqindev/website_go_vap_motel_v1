476c8b083c4db52d1e9d36450253b1bfccce2574
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-12 00:25:00 +0700

    add invoice service

--- src/backend/services/InvoiceService.js
+++ src/backend/services/InvoiceService.js
@@ -1,9 +1,12 @@
 const ApiError = require("../utils/ApiError");
 const logger = require("../utils/logger");
+const moment = require("moment-timezone");
+const RoomService = require("../services/RoomService");
+const UtilityService = require("../services/UtilityService");
 const Invoice = require("../models/Invoice");
 
 class InvoiceService {
-  static async getAllInvoices(filter) {
+  static async getAllInvoices(filter = {}) {
     try {
       logger.info(`InvoiceService.InvoiceService() is called.`);
       let query = Invoice.find();
@@ -91,20 +94,58 @@ class InvoiceService {
   static async addNewInvoice(data) {
     try {
       logger.info("InvoiceService.addNewInvoice() is called.");
-      const { roomId, invoiceCode, issueDate, dueDate } = data;
+      const { roomId, invoiceCode, issueDate, dueDate, electricity, water } =
+        data;
+      const room = await RoomService.getRoomById(roomId);
+
+      let totalAmount = 0;
 
       const isExists = await InvoiceService.isExists(roomId);
       if (isExists) throw new ApiError(409, "This Invoice already exists.");
 
       if (!invoiceCode) {
-        const room = await RoomService.getRoomById(roomId);
-        const roomNumber = room.roomNumber;
         data.invoiceCode = await InvoiceService.generateInvoiceCode(
-          roomNumber,
+          room.roomNumber,
           issueDate,
           dueDate
         );
       }
+
+      totalAmount += room.rentPrice;
+
+      if (
+        electricity?.oldIndex !== undefined &&
+        electricity?.newIndex !== undefined &&
+        electricity?.pricePerUnit
+      ) {
+        const electricityAmount =
+          (electricity.newIndex - electricity.oldIndex) *
+          electricity.pricePerUnit;
+        totalAmount += electricityAmount;
+        data.electricity.amount = electricityAmount;
+      }
+
+      if (
+        water?.oldIndex !== undefined &&
+        water?.newIndex !== undefined &&
+        water?.pricePerUnit
+      ) {
+        const waterAmount =
+          (water.newIndex - water.oldIndex) * water.pricePerUnit;
+        totalAmount += waterAmount;
+        data.water.amount = waterAmount;
+      }
+
+      if (room.utilities && room.utilities.length > 0) {
+        for (const utilityId of room.utilities) {
+          const utilityAmount = await UtilityService.getUtilityById(utilityId);
+          totalAmount += utilityAmount.price;
+        }
+      }
+
+      data.rentAmount = room.rentPrice;
+      data.utilities = room.utilities;
+      data.totalAmount = totalAmount;
 
       const newInvoice = new Invoice(data);
       const addedInvoice = await newInvoice.save();
@@ -145,12 +186,16 @@ class InvoiceService {
     }
   }
 
-  static async markInvoiceIsPaid(id) {
+  static async markInvoiceIsPaid(id, paymentMethod) {
     try {
       logger.info("InvoiceService.deleteInvoice() is called.");
       const updatedInvoice = await Invoice.findByIdAndUpdate(
         id,
-        { paymentStatus: "Paid", paymentDate: new Date() },
+        {
+          paymentStatus: "Paid",
+          paymentDate: new Date(),
+          paymentMethod: paymentMethod
+        },
         {
           new: true
         }
