331f5343904c5d194581fb4d1d73dbdb0f7e965e
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-12 00:24:18 +0700

    update connect db use async await

--- src/backend/config/databaseConfig.js
+++ src/backend/config/databaseConfig.js
@@ -1,20 +1,31 @@
 const mongoose = require("mongoose");
 const logger = require("../utils/logger");
 
-const connectMongoDB = (URI) => {
-  mongoose
-    .connect(URI, {
-      serverSelectionTimeoutMS: 5000
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true
-    })
-    .then(() => {
-      logger.info("Mongodb Cloud Atlas connected successful.");
-    })
-    .catch((error) => {
-      logger.error(`Mongodb Cloud Atlas connection have error:\n${error}`);
-      throw error;
+const connectMongoDB = async (URI) => {
+  try {
+    await mongoose.connect(URI, {
+      serverSelectionTimeoutMS: 5000,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
+    logger.info("Mongodb Cloud Atlas connected successful.");
+  } catch (error) {
+    logger.error(`Mongodb Cloud Atlas connection have error:\n${error}`);
+    throw error;
+  }
+  // mongoose
+  //   .connect(URI, {
+  //     serverSelectionTimeoutMS: 5000,
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true
+  //   })
+  //   .then(() => {
+  //     logger.info("Mongodb Cloud Atlas connected successful.");
+  //   })
+  //   .catch((error) => {
+  //     logger.error(`Mongodb Cloud Atlas connection have error:\n${error}`);
+  //     throw error;
+  //   });
 };
 
 module.exports = connectMongoDB;
