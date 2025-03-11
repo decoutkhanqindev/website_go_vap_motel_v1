2b7a1e4e3df026acf539940c3024e97dd8658591
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-11 08:22:07 +0700

    update file filter

--- src/backend/config/uploadConfig.js
+++ src/backend/config/uploadConfig.js
@@ -1,25 +1,28 @@
 const multer = require("multer");
+const ApiError = require("../utils/ApiError");
 
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
     fileSize: 5 * 1024 * 1024, // 5MB limit
-    files: 5 // Max 5 files per upload
-  }
-  // fileFilter: (req, file, cb) => {
-  //   // Allowed image MIME types
-  //   const allowedMimeTypes = ["image/png", "image/jpeg", "image/jpg"];
+    files: 5 // max 5 files per upload
+  },
+  fileFilter: (req, file, cb) => {
+    // allowed image MIME types
+    const allowedMimeTypes = ["image/png", "image/jpeg", "image/jpg"];
 
-  //   if (allowedMimeTypes.includes(file.mimetype)) {
-  //     cb(null, true); // Accept file
-  //   } else {
-  //     cb(
-  //       new Error("Invalid file type. Only PNG and JPEG images are allowed."),
-  //       false
-  //     ); // Reject file
-  //   }
-  // }
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true); // accept file
+    } else {
+      cb(
+        new ApiError(
+          "Invalid file type. Only PNG and JPEG images are allowed."
+        ),
+        false
+      ); // reject file
+    }
+  }
 });
 
 module.exports = upload;
