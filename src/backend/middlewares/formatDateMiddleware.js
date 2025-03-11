72708f315d06df19508086bb333d169df1e2b39c
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-11 08:21:17 +0700

    update more format input date

--- src/backend/middlewares/formatDateMiddleware.js
+++ src/backend/middlewares/formatDateMiddleware.js
@@ -2,16 +2,19 @@ const moment = require("moment-timezone");
 
 const formatDateMiddleware = (fields) => {
   return (req, res, next) => {
+    const dateFormats = ["DD/MM/YYYY", "DD-MM-YYYY", "YYYY-MM-DD"];
+
     for (const field of fields) {
       const dateValue =
         req.params[field] || req.query[field] || req.body[field];
 
       if (dateValue) {
-        const formattedDate = moment(dateValue, "DD/MM/YYYY", true);
+        const formattedDate = moment(dateValue, dateFormats, true);
         if (formattedDate.isValid()) {
           if (req.body[field]) req.body[field] = formattedDate.toDate();
-          if (req.query[field]) req.query[field] = formattedDate.format(); 
-          if (req.params[field]) req.params[field] = dateValue;
+          if (req.query[field]) req.query[field] = formattedDate.toISOString();
+          if (req.params[field])
+            req.params[field] = formattedDate.format("YYYY-MM-DD");
         }
       }
     }
