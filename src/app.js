8b8463f3beaa5d9f3eba788638fdde5de01c7b6f
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-03-23 00:22:52 +0700

    config static file for frontend and use client route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const connectMongoDB = require("./backend/configs/databaseConfig");
 const logger = require("./backend/utils/logger");
 const requestLoggingMiddleware = require("./backend/middlewares/requestLoggingMiddleware");
 const errorHandlingMiddleware = require("./backend/middlewares/errorHandlingMiddleware");
-const router = require("./backend/routes/apiRoutes");
+const apiRoute = require("./backend/routes/apiRoute");
+const clientRoute = require("./backend/routes/clientRoute");
 
 const app = express();
 const env = process.env;
@@ -23,8 +25,10 @@ app.use(
   })
 ); // middleware enables Cross-Origin Resource Sharing (CORS)
 app.use(cookieParser()); // middleware parses cookies attached to incoming requests
+app.use(express.static(path.join(__dirname, "frontend")));
 app.use(requestLoggingMiddleware); // middleware handle request Logging
-app.use("/api", router); // middleware handle all api routes
+app.use("/api", apiRoute); // middleware handle all api routes
+app.use("/", clientRoute); // middleware handle all client routes
 app.use(errorHandlingMiddleware); // middleware handle common error
 
 app.listen(PORT, HOSTNAME, () => {
